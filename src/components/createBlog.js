0f97930eda775b06b4b9fb9a7585fe16c9460f60
Ryan Basmajian <ryanbas21@gmail.com>
2016-12-30 16:26:13 -0800

    saving create blog component

--- src/components/createBlog.js
+++ src/components/createBlog.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const CreateBlog = props =>
 <div>
-  <textarea wrap='soft' required='required' ref=( input => store.blogText)> </textarea>
+  <textarea wrap='soft' required='required' ref={ input => store.blogText = input }> </textarea>
   <button type='submit' onClick={props.submitBlog}>Submit</button>
 </div>
 
