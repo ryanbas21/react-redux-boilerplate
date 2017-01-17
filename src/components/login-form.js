8107aa86639334fe84b69e86be984c023ab4bcc2
ryanbas21 <ryanbas21@gmail.com>
2017-01-16 19:43:26 -0800

    saving

--- src/components/login-form.js
+++ src/components/login-form.js
@@ -6,21 +6,28 @@ import Button from 'muicss/lib/react/button';
 import Row from 'muicss/lib/react/row';
 import Col from 'muicss/lib/react/col';
 import Container from 'muicss/lib/react/container';
-const LoginForm = props =>
-<Container fluid={true}>
- <form type='submit' onSubmit={props.handleLogin}>
-      <Row>
-           <Col md="12"><Input  floatingLabel={true} required={true} type='text'
-                className='log-in' id='username' label='username' /></Col></Row>
-     <Row><Col md="12"><Input  floatingLabel={true} required={true} type='password' className='log-in'
-          id='password' label='password'/></Col></Row>
-     {/* <Row><Button size="small" color="accent"
-             variant="fab" type='submit'
-             className='login-button' onClick={props.handleForm}>
-             <p className='login-text'>Login</p></Button></Row> */}
-        </form>
+const LoginForm = props => (
+<Container>
+<form type='submit' onSubmit={props.handleLogin}>
+  <legend>Title</legend>
+  <div className="mui-textfield">
+    <input type="text" required />
+    <label>Required Text Field</label>
+  </div>
+  <div className="mui-textfield mui-textfield--float-label">
+    <input type="email" required />
+    <label>Required Email Address</label>
+  </div>
+  <div className="mui-textfield mui-textfield--float-label">
+    <textarea ></textarea>
+    <label>Required Textarea</label>
+  </div>
+  <div className="mui-textfield mui-textfield--float-label">
+    <input type="email" value="Validation error" />
+    <label>Email Address</label>
+  </div>
+  <button type="submit" className="mui-btn mui-btn--raised">Submit</button>
+ </form>
 </Container>
-
-
-
+)
 module.exports = LoginForm;
