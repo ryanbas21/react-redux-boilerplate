8c169652b6f41d6411ef81cdef7f2284d5ccf24b
ryanbas21 <ryanbas21@gmail.com>
2017-01-17 10:16:10 -0800

    saving ; ;

--- src/components/Disclaimer.js
+++ src/components/Disclaimer.js
@@ -4,28 +4,30 @@ import Row from 'muicss/lib/react/row';
 import Col from 'muicss/lib/react/col';
 const Disclaimer = props =>
 <Container className='disclaimer-container-text'>
-    <header>Disclaimer</header>
     <div className='disclaimer-body'>
     <Row>
-        <p><Col fluid={true} md="4">EPB Newsletter is published as an information service.  It includes opinions as to buying, selling and holding various stocks and other securities.</Col></p>
+        <p><Col fluid={true} md="8">Disclaimer</Col></p>
+    </Row >
+    <Row>
+        <p><Col fluid={true} md="8">EPB Newsletter is published as an information service.  It includes opinions as to buying, selling and holding various stocks and other securities.</Col></p>
     </Row>
     <Row>
-        <p><Col fluid={true} md="4">However, the publishers of EPB Newsletter are not brokers or acting as investment advisers and do not provide investment advice or recommendations directed to any particular subscriber or in view of the particular circumstances of any particular person.</Col></p>
+        <p><Col fluid={true} md="8">However, the publishers of EPB Newsletter are not brokers or acting as investment advisers and do not provide investment advice or recommendations directed to any particular subscriber or in view of the particular circumstances of any particular person.</Col></p>
     </Row>
     <Row>
-        <p><Col fluid={true} md="4">EPB Newsletter does not guarantee that you will out-perform the stock market.</Col></p>
+        <p><Col fluid={true} md="8">EPB Newsletter does not guarantee that you will out-perform the stock market.</Col></p>
     </Row>
     <Row>
-        <p><Col fluid={true} md="4">The information provided by EPB Newsletter is obtained from sources believed to be reliable but is not guaranteed as to accuracy or completeness.</Col></p>
+        <p><Col fluid={true} md="8">The information provided by EPB Newsletter is obtained from sources believed to be reliable but is not guaranteed as to accuracy or completeness.</Col></p>
     </Row>
     <Row>
-        <p><Col fluid={true} md="4">At various times, the publishers and employees of EPB Newsletter may own, buy or sell the securities discussed for purposes of investment or trading.  Eric Basmajian invests his own portfolio primarily in trade ideas recommended in the EPB Newsletter.</Col></p>
+        <p><Col fluid={true} md="8">At various times, the publishers and employees of EPB Newsletter may own, buy or sell the securities discussed for purposes of investment or trading.  Eric Basmajian invests his own portfolio primarily in trade ideas recommended in the EPB Newsletter.</Col></p>
     </Row>
     <Row>
-        <p><Col fluid={true} md="4">EPB Newsletter and its publishers, owners and agents, are not liable for any losses or damages, monetary or otherwise, that result from the content of EPB Newsletter.</Col></p>
+        <p><Col fluid={true} md="8">EPB Newsletter and its publishers, owners and agents, are not liable for any losses or damages, monetary or otherwise, that result from the content of EPB Newsletter.</Col></p>
     </Row>
     <Row>
-        <p><Col fluid={true} md="4">Past results are not necessarily indicative of future performance.</Col></p>
+        <p><Col fluid={true} md="8">Past results are not necessarily indicative of future performance.</Col></p>
     </Row>
     </div>
 </Container>
