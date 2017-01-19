599062788c2e08100f7f89f83ca2ecc275399834
ryanbas21 <ryanbas21@gmail.com>
2017-01-19 15:35:14 -0800

    saving updated form wip

--- src/components/Signup-Form.js
+++ src/components/Signup-Form.js
@@ -1,9 +1,14 @@
 import React from 'react'
-
+import Form from 'muicss/lib/react/form';
+import Input from 'muicss/lib/react/input';
 const SignupForm = props =>
-    <div>Signup
-        <form></form>
-    </div>
+        <Form>
+          <legend>SignUp</legend>
+          Name:<Input label="name" floatingLabel={true} defaultValue="Name"/>
+          Email:<Input label="email" floatingLabel={true} defaultValue="Email" />
+          <Textarea label="location" floatingLabel={true} defaultValue="Location" />
+          <Button variant="raised">Submit</Button>
+        </Form>
 
 
 module.exports = SignupForm
